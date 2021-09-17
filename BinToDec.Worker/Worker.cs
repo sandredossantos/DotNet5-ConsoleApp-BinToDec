42af5a117ee408593c6dbdc279b5cdcc5ea32277
Sandré dos Santos Pereira <sandre.pereira@stone.com.br>
2021-09-16 23:12:17 -0300

    feat(WKR): criação de HostBuilder
    Criado HostBuilder
    Criado Injeção de dependência com a camada de serviços

--- BinToDec.Worker/Worker.cs
+++ BinToDec.Worker/Worker.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using BinToDec.Domain.Interfaces;
+using BinToDec.Service;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace BinToDec.Worker
 {
     public class Worker
     {
+        private readonly IConversionService _conversionService;
+
+        public Worker(IConversionService conversionService)
+        {
+            _conversionService = conversionService;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = CreateHostBuilder(args).Build();
+            host.Services.GetRequiredService<Worker>().Run();
+        }
+
+        public void Run()
+        {
+            int value = _conversionService.ConvertBinaryToDecimal("");
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddTransient<Worker>();
+                    services.AddTransient<IConversionService, ConversionService>();
+                });
         }
     }
 }
